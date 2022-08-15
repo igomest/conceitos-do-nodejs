fbaf1532e6fbb45b63482b3cb1ed53588e5e021a
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-08-15 09:14:47 -0300

    correcting errors

--- src/index.js
+++ src/index.js
@@ -27,32 +27,34 @@ function checksExistsUserAccount(request, response, next) {
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
-  const userAlreadyExists = users.some((user) => user.username === username);
+  const userAlreadyExists = users.find((user) => user.username === username);
 
   if (userAlreadyExists) {
     return response.status(400).json({ error: "User already exists" });
   }
 
-  users.push({
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: [],
-  });
+  };
 
-  return response.status(201).send();
+  users.push(user);
+
+  return response.status(201).json(user)
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
-  return response.json(user);
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
   const { title, deadline } = request.body;
 
-  const { user } = request;
 
   const createTodo = {
     id: uuidv4(),
@@ -64,12 +66,12 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 
   user.todos.push(createTodo);
 
-  return response.status(201).send();
+  return response.status(201).json(createTodo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { title, deadline } = request;
   const { user } = request;
+  const { title, deadline } = request.body;
   const { id } = request.params;
 
   const todo = user.todos.find((todo) => todo.id === id);
